0884310ff470fc698faa1cb1818fe9448de55117
kmorgport <kmorgan0057@gmail.com>
2021-08-03 20:28:38 -0500

    useEffect wth get request

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -6,23 +6,51 @@ import './App.css';
 function App() {
 
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-  const fetchMoviesHandler = () =>{
-    fetch('https://swapi.dev/api/films/')
-    .then(response =>{
-      return response.json()
-    }).then(data =>{
-      setMovies(data.results)
-    })
-  }
+  const  fetchMoviesHandler = useCallback(async ()=>{
+    setIsLoading(true)
+    setError(null);
+    //when using async await use try catch
+    try{
+      const response = await fetch('https://swapi.dev/api/films/')
+      const data = await response.json();
+      if(!response.ok){
+        throw new Error('no movies')
+      }
+      const transformedMovies = data.results.map(movieData=>{
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+      setMovies(transformedMovies)
+    }
+    catch(error){
+      setError(error.message)
+    }
+    setIsLoading(false)
+    //dont need to call any dependencies because it's not calling anything external
+    }, []);
+
+    useEffect(()=>{
+      fetchMoviesHandler();
+      //leaving it empty would possibly introduce bugs if the dependency relies on state
+    }, [])
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>There are no movies</p>}
+        {isLoading && <p>Loadng...</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
